f04e131050cc687564746f06300f5a61a1d960f3
Camilo del Real <cjdelreal2009@gmail.com>
2023-02-02 00:33:20 +0100

    FIX: Wrong service name in scripts configurations

--- scripts/image-service.bat
+++ scripts/image-service.bat
@@ -1,9 +1,9 @@
 @echo off
 @rem User Management Service
 echo [!] Imaging User Management Service
-cd ../user-management-service/
+cd ../users-management-service/
 set "dockerErr=0"
-call docker build . --tag %1/user-management-service --force-rm=true || set dockerErr=1
+call docker build . --tag %1/users-management-service --force-rm=true || set dockerErr=1
 if "%dockerErr%" == "0" echo [+] User Management Service successfully imaged
 if "%dockerErr%" == "1" echo [-] Error imaging User Management Service. Script cannot continue && goto end
 :end
